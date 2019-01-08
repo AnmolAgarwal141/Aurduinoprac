a4ef843332f8a3e9454229c3f694a779aae58e25
AnmolAgarwal141 <39688318+AnmolAgarwal141@users.noreply.github.com>
2019-01-08 19:19:13 +0530

    Add files via upload

---
+++ RPM_of_disc.ino
@@ -0,0 +1,28 @@
+int digpin=2;
+int pulse=0;
+int timetk;
+boolean lastpin=LOW;
+int rpm;
+void counter()
+{
+  pulse++;
+}
+  void setup() {
+
+  // put your setup code here, to run once:
+pinMode(digpin,INPUT);
+timetk=0;
+attachInterrupt(digitalPinToInterrupt(lastpin),counter,HIGH);
+}
+
+void loop() {
+  
+ if(millis()-timetk>=1000)
+{detachInterrupt(lastpin);
+rpm=60*(pulse-1)/2;
+timetk=millis();
+pulse=0;
+}
+attachInterrupt(digitalPinToInterrupt(lastpin),counter,CHANGE);
+//take input as low always when the hole is find it will read input as high
+}
